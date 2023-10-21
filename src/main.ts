6ea66c30e2e11df99eaf098efe865341a14f99f8
BrandonJack9 <bjack@ucsc.edu>
2023-10-21 16:33:19 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ const gameName = "ShadowHand's Game";
 
 document.title = gameName;
 
+const container = document.createElement("div");
+app.append(container);
+
 const header = document.createElement("h1");
 header.innerHTML = gameName;
-app.append(header);
+container.append(header);
+const canvas = document.createElement("canvas");
+canvas.width = 500;
+canvas.height = 300;
+container.append(canvas);
