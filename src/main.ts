d88de44ae6fc5e3829c3889954f1c95b026cb9c4
BrandonJack9 <bjack@ucsc.edu>
2023-10-21 17:22:18 -0700

    step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -16,3 +16,39 @@ const canvas = document.createElement("canvas");
 canvas.width = 500;
 canvas.height = 300;
 container.append(canvas);
+
+const twoDee = canvas.getContext("2d");
+
+const point = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener("mousedown", (e) => {
+  point.active = true;
+  point.x = e.offsetX;
+  point.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (point.active && twoDee) {
+    twoDee.beginPath();
+    twoDee.moveTo(point.x, point.y);
+    twoDee.lineTo(e.offsetX, e.offsetY);
+    twoDee.stroke();
+    point.x = e.offsetX;
+    point.y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  point.active = false;
+});
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "Clear";
+
+container.append(clearButton);
+
+clearButton.addEventListener("click", () => {
+  if (twoDee) {
+    twoDee.clearRect(0, 0, canvas.width, canvas.height);
+  }
+});
